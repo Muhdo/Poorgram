dd2129200855d562a83d81b0e5fbc83e09de0acc
Muhdo <gabrielmcv1@gmail.com>
2018-12-04 23:21:43 +0000

    Poucas mudanças no butao de registo e link para login

--- includes/registar.php
+++ includes/registar.php
@@ -8,7 +8,7 @@
          <div class="div-conteudo">
             <h1>A rede social do momento!🔥🔥🔥</h1>
             <img src="img/logo.png">
-            <h3>Não podes perder ⏰ esta forma fantástica de entenderes que nao tens amigos!</h3>
+            <h3>Não podes perder ⏰ para esta forma fantástica de entenderes que nao tens amigos!</h3>
             <h3>Acredita, eles apenas querem os teus 👍🏻</h3>
             <h3>Cria já a tua conta para ficares 😭 ao entender que ninguem dá 👍🏻 nas tuas 📷</h3>
          </div>
@@ -22,6 +22,7 @@
                <input class="form-input" id="email" type="text" name="Email" placeholder="Email Ex: JossefinoAndrade420@gamaile.come">
                <input class="form-input" id="password" type="password" name="Password" placeholder="Palavra-Passe Ex: ************">
                <input class="form-button" id="submit" type="submit" name="Registar" value="Registar">
+               <a href="#">Login</a>
             </form>
          </div>
       </div>
